41e5e08e54045aab9481f2db82d25e6be24390a2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-07 06:58:40 +0100

    playing with dimensions and parameters

--- threeJS/hello-world/src/main.js
+++ threeJS/hello-world/src/main.js
@@ -1,11 +1,12 @@
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
+console.log(scene)
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  10
 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -14,15 +15,21 @@ renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0x9cffab });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.006;
+  cube.rotation.y += 0.006;
+  cube.rotation.z += 0.004;
 
   renderer.render(scene, camera);
 }
+
+console.log(cube)
+console.log(camera);
+console.log(window.innerHeight,window.innerWidth)
+
